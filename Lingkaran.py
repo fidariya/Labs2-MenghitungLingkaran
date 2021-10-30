bd97ee832f2b17f076fda98d4a35afd1aabbe2d4
fidariya <fidamufida2004@gmail.com>
2021-10-30 22:40:33 +0700

    Menambahkan Lingkaran

---
+++ Lingkaran.py
@@ -0,0 +1,9 @@
+import math
+r = float(input("Masukan Jari-jari :"))
+
+L = math.pi*(r*r)
+K = 2*math.pi*r
+
+print("Luas Lingkaran \t= ",format(L,'.2f'))
+print("Keliling Lingkaran \t= ",format(K,'.2f'))
+
